b0b581c892bdc787fe829659d6dda1a7e3347115
Sergei Zuev <zuev56@ro.ru>
2022-04-23 08:26:24 +0300

    Add HealthCheck

---
+++ src/VkActivity.Service/Controllers/HealthCheckController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VkActivity.Service.Controllers;
+
+[Route("api/[controller]")]
+public class HealthCheckController : Controller
+{
+    [HttpGet("")]
+    [HttpHead("")]
+    public IActionResult Index()
+    {
+        if (Request.Method == "HEAD")
+            return Ok();
+
+        var currentProcess = Process.GetCurrentProcess();
+
+        return Ok(new
+        {
+            ProcessRunningTime = $"{DateTime.Now - currentProcess.StartTime:G}",
+            CpuTime = new
+            {
+                Total = currentProcess.TotalProcessorTime,
+                User = currentProcess.UserProcessorTime,
+                Priveleged = currentProcess.PrivilegedProcessorTime,
+            },
+            MemoryUsage = new
+            {
+                Current = BytesToSize(currentProcess.WorkingSet64),
+                Peak = BytesToSize(currentProcess.PeakWorkingSet64)
+            },
+            ActiveThreads = currentProcess.Threads.Count
+        });
+    }
+
+    // TODO: Move to Zs.Common
+    private static string BytesToSize(long bytes)
+    {
+        string[] array = new string[5]
+        {
+            "Bytes",
+            "KB",
+            "MB",
+            "GB",
+            "TB"
+        };
+
+        if (bytes == 0L)
+        {
+            return "0 Byte";
+        }
+
+        int num = (int)Math.Floor(Math.Log(bytes) / Math.Log(1024.0));
+        return Math.Round(bytes / Math.Pow(1024.0, num), 2) + " " + array[num];
+    }
+}
+
+
