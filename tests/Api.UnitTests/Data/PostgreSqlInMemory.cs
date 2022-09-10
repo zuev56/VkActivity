08323edd3e2c594fc27904b5b912908a1a010b80
Sergei Zuev <zuev56@ro.ru>
2022-09-10 08:26:01 +0300

    tests:  add VkActivity.Api unit tests

---
+++ tests/Api.UnitTests/Data/PostgreSqlInMemory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VkActivity.Data;
+using VkActivity.Data.Repositories;
+
+namespace UnitTests.Data;
+
+public class PostgreSqlInMemory
+{
+    public ActivityLogItemsRepository ActivityLogItemsRepository { get; }
+    public UsersRepository UsersRepository { get; }
+
+    public PostgreSqlInMemory()
+    {
+        var dbContextFactory = GetPostgreSqlBotContextFactory();
+
+        ActivityLogItemsRepository = new ActivityLogItemsRepository(dbContextFactory);
+        UsersRepository = new UsersRepository(dbContextFactory);
+    }
+
+    private VkActivityContextFactory GetPostgreSqlBotContextFactory()
+    {
+        var dbName = $"PostgreSQLInMemoryDB_{Guid.NewGuid()}";
+        var options = new DbContextOptionsBuilder<VkActivityContext>()
+            .UseInMemoryDatabase(dbName)
+            .Options;
+
+        return new VkActivityContextFactory(options);
+    }
+
+    public void FillWithFakeData(int entitiesCount)
+    {
+        var users = StubFactory.CreateUsers(entitiesCount);
+        var activityLogItems = StubFactory.CreateActivityLogItems(entitiesCount - 10);
+
+
+        Task.WaitAll(new Task[]
+        {
+            UsersRepository.SaveRangeAsync(users),
+            ActivityLogItemsRepository.SaveRangeAsync(activityLogItems)
+        });
+    }
+}
---
+++ tests/Api.UnitTests/Data/UserIdSet.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace UnitTests.Data;
+
+internal class UserIdSet
+{
+    public int InitialUsersAmount { get; }
+    public int[] InitialUserIds { get; }
+    public string[] InitialUserStringIds { get; }
+    public int[] NewUserIds { get; }
+    public string[] NewUserStringIds { get; }
+    public int[] NewAndExistingUserIds { get; }
+    public string[] NewAndExistingUserStringIds { get; }
+    public int[] ChangedExistingUserIds { get; }
+    public string[] ChangedExistingUserStringIds { get; }
+
+    private UserIdSet(int initialUsersAmount, int subluistAmountDivider)
+    {
+        InitialUsersAmount = initialUsersAmount;
+        int newUsersAmount = InitialUsersAmount / subluistAmountDivider;
+        int existingUsersAmountWhenAddNew = newUsersAmount / subluistAmountDivider;
+        int changedUsersAmount = InitialUsersAmount / subluistAmountDivider / 2;
+
+        InitialUserIds = Enumerable.Range(1, initialUsersAmount).ToArray();
+        NewUserIds = Enumerable.Range(initialUsersAmount + 1, newUsersAmount).ToArray();
+        NewAndExistingUserIds = InitialUserIds.Take(existingUsersAmountWhenAddNew).Union(NewUserIds).ToArray();
+        ChangedExistingUserIds = Enumerable.Range(1, changedUsersAmount).ToArray();
+
+        InitialUserStringIds = InitialUserIds.Select(x => x.ToString()).ToArray();
+        NewUserStringIds = NewUserIds.Select(x => x.ToString()).ToArray();
+        NewAndExistingUserStringIds = NewAndExistingUserIds.Select(x => x.ToString()).ToArray();
+        ChangedExistingUserStringIds = ChangedExistingUserIds.Select(x => x.ToString()).ToArray();
+    }
+
+    public static UserIdSet Create(int initialUsersAmount, int subluistAmountDivider = 10)
+        => new(initialUsersAmount, subluistAmountDivider);
+}
